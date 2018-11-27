90f24d0b10f1010e02433ba185857dacef709be9
JHART215 <jerome215@comcast.net>
2018-11-27 01:06:07 -0700

    Task Completed 11/27

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/module-exports-2.js
@@ -0,0 +1,11 @@
+const Airplane = {};
+
+module.exports = {
+  myAirplane: "CloudJet",
+  displayAirplane: function() {
+    return this.myAirplane;
+  }
+};
+
+const Airplane = require('./2-airplane.js');
+console.log(Airplane.displayAirplane());
